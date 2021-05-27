ffa3d393cbd0c6398b08e0acb049dc95754e19cf
VIKAS P LAL <49762419+vikasm19@users.noreply.github.com>
2021-05-27 07:46:46 +0530

    Add files via upload

---
+++ addressOfOperator.cpp
@@ -0,0 +1,22 @@
+// Address Of operator;
+
+#include <iostream>
+using namespace std;
+int main() {
+    int x = 5;
+    cout<<"Address of x: "<<&x<<endl;
+    
+    // *Limitations
+    // We can use this method to print address in all datat type instead of char.
+    //Let's see what happended in char data type;
+    char y = 'B';
+    /*
+    cout<<"Address of y: "<<&y<<endl;  */
+    
+    //This should be our code but it will print value in y isntead of it's address.
+    //So our code will be look like;
+    
+    cout<<"Address of y: "<<(void*)&y<<endl;
+    //  (void*) is just for expilict type casting from (char* to void*)
+    return 0;
+}
